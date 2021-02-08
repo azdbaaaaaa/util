df78f427e7bc91dc1bb6387580571294596d9bea
周冬彬 <zhoudongbin@bixin.cn>
2021-02-08 19:46:13 +0800

    u

--- gorm.go
+++ gorm.go
@@ -11,3 +11,45 @@ type Model struct {
 	UpdatedAt time.Time      `json:"updated_at"`
 	DeletedAt gorm.DeletedAt `gorm:"index"`
 }
+
+const (
+	maxSize     = 1000
+	defaultSize = 20
+	defaultPage = 1
+)
+
+type PaginationReq struct {
+	Search   string `form:"search" json:"search"`
+	Page     int    `form:"page" json:"page"`
+	PageSize int    `form:"page_size" json:"page_size"`
+}
+
+type PaginationRes struct {
+	PageSize int         `form:"page_size" json:"page_size"`
+	Page     int         `form:"page" json:"page"`
+	Data     interface{} `json:"data"`
+	Total    int64       `json:"total"`
+}
+
+func (p *PaginationReq) check() {
+	if p.PageSize <= 0 {
+		if p.PageSize == -1 {
+			p.PageSize = maxSize
+		} else {
+			p.PageSize = defaultSize
+		}
+	}
+	if p.Page <= 0 {
+		p.Page = defaultPage
+	}
+}
+
+func (p *PaginationReq) Offset() int {
+	p.check()
+	return (p.Page - 1) * p.PageSize
+}
+
+func (p *PaginationReq) Limit() int {
+	p.check()
+	return p.PageSize
+}
