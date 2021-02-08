cef74b90eb4b63201b1d90d6531eadbdb666a4a6
周冬彬 <zhoudongbin@bixin.cn>
2021-02-08 19:43:50 +0800

    u

---
+++ gorm.go
@@ -0,0 +1,13 @@
+package util
+
+import (
+	"gorm.io/gorm"
+	"time"
+)
+
+type Model struct {
+	ID        uint           `gorm:"primary_key" json:"id"`
+	CreatedAt time.Time      `json:"created_at"`
+	UpdatedAt time.Time      `json:"updated_at"`
+	DeletedAt gorm.DeletedAt `gorm:"index"`
+}
