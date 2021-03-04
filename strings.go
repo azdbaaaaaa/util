647b5db14125bc24b23f6601da763f8ea5403673
周冬彬 <zhoudongbin@bixin.cn>
2021-03-04 16:07:57 +0800

    u

---
+++ strings.go
@@ -0,0 +1,13 @@
+package util
+
+func removeDuplicate(input []string) (output []string) {
+	output = make([]string, 0, len(input))
+	temp := map[string]struct{}{}
+	for _, item := range input {
+		if _, ok := temp[item]; !ok {
+			temp[item] = struct{}{}
+			output = append(output, item)
+		}
+	}
+	return output
+}
