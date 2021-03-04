6e9b534d8a67541c6604bcca847d5b16bceea524
周冬彬 <zhoudongbin@bixin.cn>
2021-03-04 16:19:28 +0800

    u

--- strings.go
+++ strings.go
@@ -1,6 +1,6 @@
 package util
 
-func removeDuplicateString(input []string) (output []string) {
+func RemoveDuplicateStrings(input []string) (output []string) {
 	output = make([]string, 0, len(input))
 	temp := map[string]struct{}{}
 	for _, item := range input {
