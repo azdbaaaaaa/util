88b609f96bfb4c7cdd7ce752641c3b4d8b4b456e
周冬彬 <zhoudongbin@ficool.com>
2021-04-27 18:02:14 +0800

    update

--- storage/mysql/model.go
+++ storage/mysql/model.go
@@ -6,7 +6,7 @@ import (
 
 type Model struct {
 	ID          uint           `gorm:"primary_key" json:"id"`
-	CreateTime  int64          `gorm:"default:CURRENT_TIMESTAMP" json:"create_time"`
-	UpdateTime  int64          `gorm:"default:CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP" json:"update_time"`
+	CreateTime  int64          `gorm:"type:timestamp;default:CURRENT_TIMESTAMP" json:"create_time"`
+	UpdateTime  int64          `gorm:"type:timestamp;default:CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP" json:"update_time"`
 	DeletedTime gorm.DeletedAt `gorm:"index" json:"-"`
 }
