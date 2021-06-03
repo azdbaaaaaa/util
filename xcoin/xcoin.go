8e161344e32112a888411384b8cf6926edc99844
周冬彬 <zhoudongbin@ficool.com>
2021-06-03 15:53:10 +0800

    update

--- xcoin/xcoin.go
+++ xcoin/xcoin.go
@@ -1,7 +1,5 @@
 package xcoin
 
-import "errors"
-
 const CoinBase int64 = 10000
 
 type Coin int64
@@ -10,9 +8,6 @@ func (c Coin) Set() int64 {
 	return int64(c) * CoinBase
 }
 
-func (c Coin) Get() (int64, error) {
-	if int64(c)%CoinBase != 0 {
-		return 0, errors.New("coin is invalid")
-	}
-	return int64(c) / CoinBase, nil
+func (c Coin) Get() float32 {
+	return float32(int64(c) / CoinBase)
 }
