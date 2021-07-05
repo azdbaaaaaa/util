dc145929783f8d58f44f9045cdc91046aa379b37
周冬彬 <zhoudongbin@ficool.com>
2021-07-05 10:56:35 +0800

    update

--- net/middleware/in_param/gin.go
+++ net/middleware/in_param/gin.go
@@ -1,14 +1,15 @@
 package in_param
 
 import (
+	"github.com/azdbaaaaaa/util/proto"
 	"github.com/gin-gonic/gin"
-	uuid "github.com/satori/go.uuid"
 )
 
-func SetInParam() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		reqID := uuid.NewV4().String()
-		c.Set(ContextKeyInParam, reqID)
-		c.Next()
-	}
+func SetInParam(c *gin.Context) {
+	c.Set(ContextKeyInParam, proto.InParam{
+		AreaId:   int32(proto.AppIdEnum_LIGHTHOUSE),
+		ClientIp: c.ClientIP(),
+		//UserId: c.Get(ContextKeyUser),
+	})
+	c.Next()
 }
