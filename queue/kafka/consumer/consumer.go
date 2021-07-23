88ce25e88cbf5cf755ee6f00cfcd39e0cbb15a4a
yuhuibin <yuhuibin@ficool.com>
2021-07-23 17:08:24 +0800

    update kafka consumer

--- queue/kafka/consumer/consumer.go
+++ queue/kafka/consumer/consumer.go
@@ -1,7 +1,6 @@
 package consumer
 
 import (
-	"context"
 	"github.com/Shopify/sarama"
 	"github.com/azdbaaaaaa/util/log"
 	"strings"
@@ -16,73 +15,7 @@ type Config struct {
 	AutoCommit int32  `json:"auto_commit" mapstructure:"auto_commit"` //是否自动提交偏移量, 0:true, 1:false
 }
 
-type ConsumerGroup struct {
-	group sarama.ConsumerGroup
-}
-
-func NewConsumerGroup(brokers []string, groupId string, offset int64, autoCommit bool) *ConsumerGroup {
-	config := sarama.NewConfig()
-	config.Version = sarama.V2_2_0_0
-	config.Consumer.Group.Rebalance.Strategy = sarama.BalanceStrategySticky
-	config.Consumer.Group.Session.Timeout = 20 * time.Second
-	config.Consumer.Group.Heartbeat.Interval = 6 * time.Second
-	config.Consumer.IsolationLevel = sarama.ReadCommitted
-	config.Consumer.Offsets.AutoCommit.Enable = autoCommit
-	config.Consumer.Offsets.Initial = offset
-	consumerGroup, err := sarama.NewConsumerGroup(brokers, groupId, config)
-	if err != nil {
-		log.Panicf("error to NewConsumerGroup %v", err)
-		return nil
-	}
-	return &ConsumerGroup{group: consumerGroup}
-}
-
-func (CG *ConsumerGroup) Consume(topics []string, ctx context.Context, handler ConsumerGroupHandler) {
-	defer CG.group.Close()
-	for {
-		if err := CG.group.Consume(ctx, topics, handler); err != nil {
-			log.Errorw("ConsumerGroup.Consume", "err", err)
-		}
-		if ctx.Err() != nil {
-			log.Errorw("ctx.Err", ctx.Err())
-			return
-		}
-	}
-}
-
-/*type ConsumerGroupHandler struct {
-	cs ConsumerService
-}
-
-func (CGH *ConsumerGroupHandler) Setup(sarama.ConsumerGroupSession) error {
-	return nil
-}
-
-func (CGH *ConsumerGroupHandler) Cleanup(sarama.ConsumerGroupSession) error {
-	return nil
-}
-
-func (CGH *ConsumerGroupHandler) ConsumeClaim(session sarama.ConsumerGroupSession, claim sarama.ConsumerGroupClaim) error {
-	log.Infof("Message claimed:  topic = %s, partitions = %d", claim.Topic(), claim.Partition())
-	for message := range claim.Messages() {
-		err := CGH.cs.ReceiveMessages(message)
-		if err != nil {
-			log.Errorf("ConsumerGroupHandler.ReceiveMessages: %s, error: %v", string(message.Value), err)
-		} else {
-			session.MarkMessage(message, "")
-		}
-	}
-	return nil
-}
-*/
-type ConsumerGroupHandler interface {
-	//ReceiveMessages(message *sarama.ConsumerMessage) (err error)
-	Setup(sarama.ConsumerGroupSession) error
-	Cleanup(sarama.ConsumerGroupSession) error
-	ConsumeClaim(session sarama.ConsumerGroupSession, claim sarama.ConsumerGroupClaim) error
-}
-
-func Run(ctx context.Context, conf *Config, handler ConsumerGroupHandler) {
+func NewConsumerGroup(conf *Config) sarama.ConsumerGroup {
 	var (
 		offset     int64
 		autoCommit bool
@@ -105,7 +38,18 @@ func Run(ctx context.Context, conf *Config, handler ConsumerGroupHandler) {
 	default:
 		autoCommit = true
 	}
-	cg := NewConsumerGroup(brokers, conf.Group, offset, autoCommit)
-	go cg.Consume([]string{conf.Topic}, ctx, handler)
-	return
+	config := sarama.NewConfig()
+	config.Version = sarama.V2_2_0_0
+	config.Consumer.Group.Rebalance.Strategy = sarama.BalanceStrategySticky
+	config.Consumer.Group.Session.Timeout = 20 * time.Second
+	config.Consumer.Group.Heartbeat.Interval = 6 * time.Second
+	config.Consumer.IsolationLevel = sarama.ReadCommitted
+	config.Consumer.Offsets.AutoCommit.Enable = autoCommit
+	config.Consumer.Offsets.Initial = offset
+	consumerGroup, err := sarama.NewConsumerGroup(brokers, conf.Group, config)
+	if err != nil {
+		log.Panicf("error to NewConsumerGroup %v", err)
+		return nil
+	}
+	return consumerGroup
 }
