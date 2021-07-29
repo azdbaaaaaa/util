38f35a3bf79bdf42af4e42443a8c9b3b7cd816f7
周冬彬 <zhoudongbin@ficool.com>
2021-07-29 17:03:04 +0800

    update

--- xutil/xerror/errors.go
+++ xutil/xerror/errors.go
@@ -5,7 +5,7 @@ import (
 )
 
 var (
-	Success         = New(int32(common.ErrCode_success), int32(common.ErrCode_unknown_error), "success", "")
-	ErrUnknown      = New(int32(common.ErrCode_unknown_error), int32(common.ErrCode_unknown_error), common.ErrCode_unknown_error.String(), "")
-	ErrParamInvalid = New(int32(common.ErrCode_server_param_invalid), int32(common.ErrCode_unknown_error), common.ErrCode_server_param_invalid.String(), "")
+	Success         = New(int32(common.ErrCode_success), int32(common.ErrCode_unknown_error), "success")
+	ErrUnknown      = New(int32(common.ErrCode_unknown_error), int32(common.ErrCode_unknown_error), common.ErrCode_unknown_error.String())
+	ErrParamInvalid = New(int32(common.ErrCode_server_param_invalid), int32(common.ErrCode_unknown_error), common.ErrCode_server_param_invalid.String())
 )
