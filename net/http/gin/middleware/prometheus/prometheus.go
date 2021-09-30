745b0ab575b1d015fc78721185cd45b8b7afa100
周冬彬 <zhoudongbin@ficool.com>
2021-09-30 16:42:46 +0800

    update prometheus

--- net/http/gin/middleware/prometheus/prometheus.go
+++ net/http/gin/middleware/prometheus/prometheus.go
@@ -54,7 +54,7 @@ var (
 			Name:        "request_duration",
 			Help:        "the time server took to handle the request",
 			ConstLabels: ConstLabels,
-			Buckets:     []float64{0.1, 0.2, 0.3, 0.4, 0.5, 1, 2, 3, 5, 10, 30, 60},
+			Buckets:     []float64{0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.1, 0.2, 0.3, 0.4, 0.5, 1, 2, 3, 5, 10, 30, 60},
 		},
 		[]string{"path", "method", "code", "ip"})
 
