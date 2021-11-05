96cdf60c178e3c1c49d968bfabde98cb6ca85d8d
周冬彬 <zhoudongbin@ficool.com>
2021-11-05 14:15:07 +0800

    update user_id in in_param

--- net/metadata/in_param.go
+++ net/metadata/in_param.go
@@ -11,15 +11,15 @@ const (
 )
 
 type InParam struct {
-	AppId common.AppIdType `json:"app_id,omitempty"`
-	//AreaId   proto.AreaIdType `json:"area_id,omitempty"`
-	//Version  int32            `json:"version,omitempty"`
-	UserId int64 `json:"user_id,omitempty"`
-	//DeviceId string           `json:"device_id,omitempty"`
-	ClientIp string `json:"client_ip,omitempty"`
+	AppId    common.AppIdType `json:"app_id,omitempty"`
+	UserIp   string           `json:"user_ip,omitempty"`
+	UserId   int64            `json:"user_id,omitempty"`
+	ClientIp string           `json:"client_ip,omitempty"`
 	//Channel  string           `json:"channel,omitempty"`
 	//Source   string           `json:"source,omitempty"`
-	UserIp string `json:"user_ip,omitempty"`
+	//DeviceId string           `json:"device_id,omitempty"`
+	//AreaId   proto.AreaIdType `json:"area_id,omitempty"`
+	//Version  int32            `json:"version,omitempty"`
 	//Language string           `json:"language,omitempty"`
 	//Country  string           `json:"country,omitempty"`
 	//AuthorId int64            `json:"author_id,omitempty"`
