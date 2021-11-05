317786173b9356be1eb00c2a5a217c81fb2d0344
周冬彬 <zhoudongbin@ficool.com>
2021-11-05 14:56:17 +0800

    update in param

--- net/metadata/in_param.go
+++ net/metadata/in_param.go
@@ -15,14 +15,6 @@ type InParam struct {
 	UserIp   string           `json:"user_ip,omitempty"`
 	UserId   int64            `json:"user_id,omitempty"`
 	ClientIp string           `json:"client_ip,omitempty"`
-	//Channel  string           `json:"channel,omitempty"`
-	//Source   string           `json:"source,omitempty"`
-	//DeviceId string           `json:"device_id,omitempty"`
-	//AreaId   proto.AreaIdType `json:"area_id,omitempty"`
-	//Version  int32            `json:"version,omitempty"`
-	//Language string           `json:"language,omitempty"`
-	//Country  string           `json:"country,omitempty"`
-	//AuthorId int64            `json:"author_id,omitempty"`
 }
 
 func InParamFromContext(ctx context.Context) (i InParam, err error) {
