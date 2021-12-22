cef7cecf6d2457b5bc14699a3a76c63a67adda0b
周冬彬 <zhoudongbin@ficool.com>
2021-12-22 14:30:11 +0800

    update xmap

--- xutil/xmap/xmap.go
+++ xutil/xmap/xmap.go
@@ -1,8 +1,21 @@
 package xmap
 
+import "strconv"
+
 func KeyString(m map[string]string, k string) string {
 	if v, ok := m[k]; ok {
 		return v
 	}
 	return ""
+}
+
+func KeyInt64(m map[string]string, k string) int64 {
+	if v, ok := m[k]; ok {
+		i, err := strconv.ParseInt(v, 10, 64)
+		if err != nil {
+			return 0
+		}
+		return i
+	}
+	return 0
 }
