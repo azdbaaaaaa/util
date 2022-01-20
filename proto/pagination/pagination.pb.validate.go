a00edda1be3d71f6639ea5ff5c9c96a746940b83
周冬彬 <zhoudongbin@ficool.com>
2022-01-20 15:33:48 +0800

    update time range

--- proto/pagination/pagination.pb.validate.go
+++ proto/pagination/pagination.pb.validate.go
@@ -113,3 +113,81 @@ var _ interface {
 	Cause() error
 	ErrorName() string
 } = PaginationReqValidationError{}
+
+// Validate checks the field values on TimeRange with the rules defined in the
+// proto definition for this message. If any rules are violated, an error is returned.
+func (m *TimeRange) Validate() error {
+	if m == nil {
+		return nil
+	}
+
+	if m.GetStart() <= 0 {
+		return TimeRangeValidationError{
+			field:  "Start",
+			reason: "value must be greater than 0",
+		}
+	}
+
+	if m.GetEnd() <= 0 {
+		return TimeRangeValidationError{
+			field:  "End",
+			reason: "value must be greater than 0",
+		}
+	}
+
+	return nil
+}
+
+// TimeRangeValidationError is the validation error returned by
+// TimeRange.Validate if the designated constraints aren't met.
+type TimeRangeValidationError struct {
+	field  string
+	reason string
+	cause  error
+	key    bool
+}
+
+// Field function returns field value.
+func (e TimeRangeValidationError) Field() string { return e.field }
+
+// Reason function returns reason value.
+func (e TimeRangeValidationError) Reason() string { return e.reason }
+
+// Cause function returns cause value.
+func (e TimeRangeValidationError) Cause() error { return e.cause }
+
+// Key function returns key value.
+func (e TimeRangeValidationError) Key() bool { return e.key }
+
+// ErrorName returns error name.
+func (e TimeRangeValidationError) ErrorName() string { return "TimeRangeValidationError" }
+
+// Error satisfies the builtin error interface
+func (e TimeRangeValidationError) Error() string {
+	cause := ""
+	if e.cause != nil {
+		cause = fmt.Sprintf(" | caused by: %v", e.cause)
+	}
+
+	key := ""
+	if e.key {
+		key = "key for "
+	}
+
+	return fmt.Sprintf(
+		"invalid %sTimeRange.%s: %s%s",
+		key,
+		e.field,
+		e.reason,
+		cause)
+}
+
+var _ error = TimeRangeValidationError{}
+
+var _ interface {
+	Field() string
+	Reason() string
+	Key() bool
+	Cause() error
+	ErrorName() string
+} = TimeRangeValidationError{}
