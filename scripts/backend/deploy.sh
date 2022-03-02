553e7f760992f5f8e75b0ec375159498f8d8075b
周冬彬 <zhoudongbin@ficool.com>
2022-03-02 17:22:55 +0800

    k8s

--- scripts/backend/deploy.sh
+++ scripts/backend/deploy.sh
@@ -52,7 +52,7 @@ deploy_k8s() {
       echo 2
     fi
 
-    if [[ $INIT -eq "" ]];then
+    if [[ $INIT == "" ]];then
       echo "replace configmap, deployment"
       kubectl create configmap "${DEPLOYMENT}" --from-file=${DEPLOYMENT}.yaml="config/${PROJECT}-${ENV}.yaml" -n "${NAMESPACE}" -o yaml --dry-run=client | kubectl replace -f -
       kubectl set image "deployment/${DEPLOYMENT}" ${DEPLOYMENT}="$IMAGE_REPO/$PROJECT:$VERSION" -n "${NAMESPACE}"
