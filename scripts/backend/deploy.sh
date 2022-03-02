196a11ed3813553fe866d520948a61fec799ecf2
周冬彬 <zhoudongbin@ficool.com>
2022-03-02 17:04:40 +0800

    k8s

--- scripts/backend/deploy.sh
+++ scripts/backend/deploy.sh
@@ -6,23 +6,6 @@ export VERSION
 IMAGE_REPO=${IMAGE_REPO}
 export IMAGE_REPO
 
-
-yaml_deployment(){
-  export PORT=$PORT
-  wget https://raw.githubusercontent.com/azdbaaaaaa/util/master/scripts/k8s/${ENV}/${TYPE}/deployment.template.yaml
-  cp -a deployment.template.yaml deployment.yaml
-  file=`cat deployment.yaml`
-  printf "`export -p`\ncat << EOF\n$file\nEOF" | bash > deployment.yaml
-}
-
-yaml_service(){
-  export PORT=$PORT
-  wget https://raw.githubusercontent.com/azdbaaaaaa/util/master/scripts/k8s/${ENV}/${TYPE}/service.template.yaml
-  cp -a service.template.yaml service.yaml
-  file=`cat service.yaml`
-  printf "`export -p`\ncat << EOF\n$file\nEOF" | bash > service.yaml
-}
-
 deploy(){
     ENV=$1
     HOST=$2
@@ -50,16 +33,51 @@ deploy_k8s() {
     export ENV=$1
     export NAMESPACE=$2
 
-    echo "开始发布，环境:${ENV}"
+    echo "开始发布，环境:${ENV}", "type",$TYPE,"init", $INIT, "cmd", $CMD
     if [[ ${CMD} == "serve" ]]
     then
       export DEPLOYMENT="${PROJECT}"
+      export SERVICE="${PROJECT}"
     else
       export DEPLOYMENT="${PROJECT}-${CMD}"
+      export SERVICE="${PROJECT}-${CMD}"
     fi
-    echo "init", $INIT
-    kubectl create configmap "${DEPLOYMENT}" --from-file=${DEPLOYMENT}.yaml="config/${PROJECT}-${ENV}.yaml" -n "${NAMESPACE}" -o yaml --dry-run=client | kubectl replace -f -
-    kubectl set image "deployment/${DEPLOYMENT}" ${DEPLOYMENT}="$IMAGE_REPO/$PROJECT:$VERSION" -n "${NAMESPACE}"
+    echo "DEPLOYMENT",$DEPLOYMENT,"SERVICE",$SERVICE
+
+    ## k8s configmap
+    if [[ ${INIT} -eq "" ]];then
+      kubectl create configmap "${DEPLOYMENT}" --from-file=${DEPLOYMENT}.yaml="config/${PROJECT}-${ENV}.yaml" -n "${NAMESPACE}" -o yaml --dry-run=client | kubectl replace -f -
+      kubectl set image "deployment/${DEPLOYMENT}" ${DEPLOYMENT}="$IMAGE_REPO/$PROJECT:$VERSION" -n "${NAMESPACE}"
+    else
+      kubectl create configmap "${DEPLOYMENT}" --from-file=${DEPLOYMENT}.yaml="config/${PROJECT}-${ENV}.yaml" -n "${NAMESPACE}"
+
+      case "${TYPE}" in
+      http)
+        PORT=`kubectl get configmap ${DEPLOYMENT} -n "${NAMESPACE}" -o json | jq -r ".data.\"$DEPLOYMENT.yaml\"" | yq e '.http.addr' - | sed 's/://g'`
+        if [[ ${PORT} -eq "" ]];then
+          exit 1
+        fi
+        export PORT=$PORT
+        ;;
+      grpc)
+        PORT=`kubectl get configmap ${DEPLOYMENT} -n "${NAMESPACE}" -o json | jq -r ".data.\"$DEPLOYMENT.yaml\"" | yq e '.grpc.addr' - | sed 's/://g'`
+        if [[ ${PORT} -eq "" ]];then
+          exit 1
+        fi
+        export PORT=$PORT
+        ;;
+      job)
+        ;;
+      esac
+
+      git clone https://github.com/azdbaaaaaa/util.git --depth=1
+      kustomize build util/scripts/k8s/${TYPE}/overlays/${ENV} > all.yaml
+      file=`cat all.yaml`
+      printf "`export -p`\ncat << EOF\n$file\nEOF" | bash > all.yaml
+      cat all.yaml
+      kubectl apply -f all.yaml
+    fi
+
 }
 
 case ${CI_COMMIT_REF_NAME} in
