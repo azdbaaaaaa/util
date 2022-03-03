d7296f12475f399c1315e441ff25abe7b01b168e
周冬彬 <zhoudongbin@ficool.com>
2022-03-03 14:36:09 +0800

    k8s

--- scripts/backend/deploy.sh
+++ scripts/backend/deploy.sh
@@ -74,14 +74,18 @@ deploy_k8s() {
         fi
         export PORT=$PORT
         ;;
+      consumer)
+        ;;
+      cronjob)
+        ;;
       *)
-        echo "job continue"
+        echo "${TYPE} continue"
         ;;
       esac
 
       echo "kustomize yaml"
       git clone https://github.com/azdbaaaaaa/util.git --depth=1
-      kustomize build util/scripts/k8s/${TYPE}/overlays/${ENV} > all.yaml
+      kustomize build util/scripts/k8s/app/${TYPE}/overlays/${ENV} > all.yaml
       file=`cat all.yaml`
       printf "`export -p`\ncat << EOF\n$file\nEOF" | bash > all.yaml
       cat all.yaml
