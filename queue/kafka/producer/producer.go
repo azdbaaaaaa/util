8d5f8db1b07969d72e58135362578a4668847380
周冬彬 <zhoudongbin@ficool.com>
2022-04-02 11:03:37 +0800

    add producer

--- queue/kafka/producer/producer.go
+++ queue/kafka/producer/producer.go
@@ -7,13 +7,17 @@ import (
 )
 
 type Config struct {
-	Broker string `json:"broker"` // 以,分割的列表
-	Topic  string `json:"topic"`
+	Broker       string              `json:"broker"` // 以,分割的列表
+	Topic        string              `json:"topic"`
+	RequiredAcks sarama.RequiredAcks `json:"required_acks" mapstructure:"required_acks"`
 }
 
 func NewProducer(conf *Config) (producer sarama.SyncProducer, err error) {
 	config := sarama.NewConfig()
 	config.Producer.Return.Successes = true
+	if conf.RequiredAcks != 0 {
+		config.Producer.RequiredAcks = conf.RequiredAcks
+	}
 	producer, err = sarama.NewSyncProducer(strings.Split(conf.Broker, ","), config)
 	if err != nil {
 		log.Errorf("error to create sync producer: %v", err)
