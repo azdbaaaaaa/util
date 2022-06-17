0ea09e6d9573646426f0b5809af99da3587dfe02
周冬彬 <zhoudongbin@ficool.com>
2022-06-17 14:58:52 +0800

    update cicd

--- scripts/backend/deploy.sh
+++ scripts/backend/deploy.sh
@@ -60,93 +60,39 @@ deploy_k8s() {
     export ENV=$1
     export NAMESPACE=$2
 
-    echo "开始发布，环境:",${ENV},'$NAMESPACE',$NAMESPACE,'$TYPE',$TYPE,'$INIT',$INIT,'$CMD',$CMD
-    if [[ ${CMD} == "serve" ]]
-    then
-      export DEPLOYMENT="${PROJECT}"
-      export SERVICE="${PROJECT}"
-      export CRONJOB="${PROJECT}"
-      export JOB="${PROJECT}"
-    else
-      export DEPLOYMENT="${PROJECT}-${CMD}"
-      export SERVICE="${PROJECT}-${CMD}"
-      export CRONJOB="${PROJECT}-${CMD}"
-      export JOB="${PROJECT}-${CMD}"
-    fi
-    echo "DEPLOYMENT",$DEPLOYMENT,"SERVICE",$SERVICE,"CRONJOB",$CRONJOB,"SCHEDULE",$SCHEDULE,"JOB",$JOB
+    # 更新/新建 configmap
+    kubectl create configmap "${PROJECT}-${CMD}" --from-file=${PROJECT}-${CMD}.yaml="config/${PROJECT}-${ENV}.yaml" -n "${NAMESPACE}" -o yaml --dry-run=client | kubectl replace -f - || \
+    kubectl create configmap "${PROJECT}-${CMD}" --from-file=${PROJECT}-${CMD}.yaml="config/${PROJECT}-${ENV}.yaml" -n "${NAMESPACE}"
 
+    export HTTP_PORT=`kubectl get configmap "${PROJECT}-${CMD}" -n "${NAMESPACE}" -o json | jq -r ".data.\"${PROJECT}-${CMD}.yaml\"" | yq e '.http.addr' - | sed 's/://g'`
+    export GRPC_PORT=`kubectl get configmap "${PROJECT}-${CMD}" -n "${NAMESPACE}" -o json | jq -r ".data.\"${PROJECT}-${CMD}.yaml\"" | yq e '.grpc.addr' - | sed 's/://g'`
+    export SCHEDULE=`kubectl get configmap "${PROJECT}-${CMD}" -n "${NAMESPACE}" -o json | jq -r ".data.\"${PROJECT}-${CMD}.yaml\"" | yq e ".cronjob.${CMD}.schedule" -`
 
+    echo "开始发布，环境:",${ENV},'$NAMESPACE',$NAMESPACE,'$TYPE',$TYPE,'$INIT',$INIT,'$CMD',$CMD,'$HTTP_PORT',$HTTP_PORT,'$GRPC_PORT',$GRPC_PORT,'$SCHEDULE',$SCHEDULE
 
-    ## k8s configmap
     if [[ $INIT == "" ]];then
-      kubectl create configmap "${DEPLOYMENT}" --from-file=${DEPLOYMENT}.yaml="config/${PROJECT}-${ENV}.yaml" -n "${NAMESPACE}" -o yaml --dry-run=client | kubectl replace -f -
-      echo "configmap updated === kubectl create configmap ${DEPLOYMENT} --from-file=${DEPLOYMENT}.yaml=\"config/${PROJECT}-${ENV}.yaml\" -n ${NAMESPACE} -o yaml --dry-run=client"
       case "${TYPE}" in
-      job)
-        exit 1
-        ;;
-      cronjob)
-        # TODO
-        exit 1
-        ;;
-      *)
+      consumer|http|grpc)
         kubectl set image "deployment/${DEPLOYMENT}" ${DEPLOYMENT}="$IMAGE_REPO/$PROJECT:$VERSION" -n "${NAMESPACE}"
         echo "deployment updated"
+        exit 0
+        echo "already return"
         ;;
       esac
-    else
-      echo "create configmap, deployment, service, etc..."
-      kubectl create configmap "${DEPLOYMENT}" --from-file=${DEPLOYMENT}.yaml="config/${PROJECT}-${ENV}.yaml" -n "${NAMESPACE}"
-      echo "configmap created"
-      case "${TYPE}" in
-      http)
-        PORT=`kubectl get configmap ${DEPLOYMENT} -n "${NAMESPACE}" -o json | jq -r ".data.\"$DEPLOYMENT.yaml\"" | yq e '.http.addr' - | sed 's/://g'`
-        echo "port",$PORT
-        if [[ ${PORT} -eq "" ]];then
-          exit 1
-        fi
-        export PORT=$PORT
-        ;;
-      grpc)
-        PORT=`kubectl get configmap ${DEPLOYMENT} -n "${NAMESPACE}" -o json | jq -r ".data.\"$DEPLOYMENT.yaml\"" | yq e '.grpc.addr' - | sed 's/://g'`
-        echo "port",$PORT
-        if [[ ${PORT} -eq "" ]];then
-          exit 1
-        fi
-        export PORT=$PORT
-        ;;
-      consumer)
-        ;;
-      cronjob)
-        SCHEDULE=`kubectl get configmap ${CRONJOB} -n "${NAMESPACE}" -o json | jq -r ".data.\"$CRONJOB.yaml\"" | yq e ".cronjob.${CMD}.schedule" -`
-        echo "SCHEDULE,$SCHEDULE,end"
-        if [[ "$SCHEDULE" == "" ]];then
-          exit 1
-        fi
-        export SCHEDULE="$SCHEDULE"
-        ;;
-      job)
-        ;;
-      *)
-        echo "${TYPE} not supported"
-        exit 1
-        ;;
-      esac
-
-      echo "kustomize yaml"
-      git clone https://github.com/azdbaaaaaa/util.git --depth=1
-      echo "clone util finished"
-      kustomize build util/scripts/k8s/app/${TYPE}/overlays/${ENV} > all.yaml
-      echo "kustomize build finished"
-      file=`cat all.yaml`
-      export -p
-      echo "export -p"
-      printf "`export -p`\ncat << EOF\n$file\nEOF" | bash > all.yaml
-      cat all.yaml
-      echo "cat all.yaml"
-      kubectl apply -f all.yaml
     fi
 
+    echo "kustomize yaml"
+    git clone https://github.com/azdbaaaaaa/util.git --depth=1
+    echo "clone util finished"
+    kustomize build util/scripts/k8s/app/${TYPE}/overlays/${ENV} > all.yaml
+    echo "kustomize build finished"
+    file=`cat all.yaml`
+    export -p
+    echo "export -p"
+    printf "`export -p`\ncat << EOF\n$file\nEOF" | bash > all.yaml
+    cat all.yaml
+    echo "cat all.yaml"
+    kubectl apply -f all.yaml
 }
 
 case ${CI_COMMIT_REF_NAME} in
