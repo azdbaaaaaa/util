e056eaeaec196890ecfd81cb365d3f6ba5eb623d
周冬彬 <zhoudongbin@ficool.com>
2022-06-29 18:13:22 +0800

    update proto

--- proto/common/response.pb.go
+++ proto/common/response.pb.go
@@ -16,7 +16,7 @@ package common
 import (
 	protoreflect "google.golang.org/protobuf/reflect/protoreflect"
 	protoimpl "google.golang.org/protobuf/runtime/protoimpl"
-	anypb "google.golang.org/protobuf/types/known/anypb"
+	anypb "github.com/gogo/protobuf/types"
 	reflect "reflect"
 	sync "sync"
 )
