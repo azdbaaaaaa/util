46893b59261d34ad554a6a02bb4de1a36deff8a2
周冬彬 <zhoudongbin@ficool.com>
2022-06-30 18:18:38 +0800

    update proto

--- Makefile
+++ Makefile
@@ -14,7 +14,7 @@ breaking:format
 generate:breaking
 	cd proto && buf mod update && cd ..
 	buf generate
-	sed -i.bak "s/google.golang.org\/protobuf\/types\/known\/anypb/github.com\/gogo\/protobuf\/types/g" proto/common/response.pb.go && rm proto/common/response.pb.go.bak
+	sh proto/gen-proto.sh
 
 push:generate
 	cd proto && buf push && cd ..
--- proto/gen-proto.sh
+++ proto/gen-proto.sh
@@ -8,17 +8,19 @@ cd "$home"
 #cd $home && cd src/proto
 #protoc --gofast_out=plugins=grpc:. *.proto
 proto_path=.:${GOPATH}/src/
+proto_path=${proto_path}:${GOPATH}/src/github.com/gogo/protobuf/
 proto_path=${proto_path}:${GOPATH}/src/github.com/gogo/protobuf/protobuf/
 proto_path=${proto_path}:${GOPATH}/src/github.com/envoyproxy/protoc-gen-validate/
 proto_path=${proto_path}:${GOPATH}/src/github.com/azdbaaaaaa/util/
 
 
 cd "${home}"/proto/common
-protoc --gofast_out=plugins=grpc:. --proto_path=${proto_path} --validate_out="lang=go:." common.proto
+protoc --go_out=. --proto_path=${proto_path} *.proto
 mv "${home}"/proto/common/github.com/azdbaaaaaa/util/proto/common/* "${home}"/proto/common
+sed -i.bak "s/google.golang.org\/protobuf\/types\/known\/anypb/github.com\/gogo\/protobuf\/types/g" common.pb.go && rm common.pb.go.bak
 rm -r "${home}"/proto/common/github.com/
 
-cd "${home}"/proto/pagination
-protoc --gofast_out=plugins=grpc:. --proto_path=${proto_path} --validate_out="lang=go:." pagination.proto
-mv "${home}"/proto/pagination/github.com/azdbaaaaaa/util/proto/pagination/* "${home}"/proto/pagination
-rm -r "${home}"/proto/pagination/github.com/
+#cd "${home}"/proto/pagination
+#protoc --gofast_out=plugins=grpc:. --proto_path=${proto_path} --validate_out="lang=go:." pagination.proto
+#mv "${home}"/proto/pagination/github.com/azdbaaaaaa/util/proto/pagination/* "${home}"/proto/pagination
+#rm -r "${home}"/proto/pagination/github.com/
