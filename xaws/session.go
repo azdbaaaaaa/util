0aa849ad2044a5d5c9d0d85d2392426158e506dc
周冬彬 <zhoudongbin@ficool.com>
2022-07-01 18:21:09 +0800

    aws session

--- xaws/session.go
+++ xaws/session.go
@@ -4,18 +4,15 @@ import (
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/s3"
 )
 
 // NewSession returns a new session
-func NewSession(c *Config) (cli *s3.S3) {
+func NewSession(c *Config) (sess *session.Session) {
 	creds := credentials.NewStaticCredentials(c.AccessKey, c.SecretKey, "")
 	conf := &aws.Config{
 		Credentials:      creds,
 		Region:           aws.String(c.Region),
 		S3ForcePathStyle: aws.Bool(true),
 	}
-	sess := session.Must(session.NewSession(conf))
-	cli = s3.New(sess)
-	return
+	return session.Must(session.NewSession(conf))
 }
