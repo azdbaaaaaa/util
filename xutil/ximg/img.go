dd109c513a9cd68dee2018367a6458bc0de64b89
周冬彬 <zhoudongbin@ficool.com>
2022-07-05 11:38:53 +0800

    update ximg

--- xutil/ximg/img.go
+++ xutil/ximg/img.go
@@ -2,6 +2,7 @@ package ximg
 
 import (
 	"context"
+	"encoding/base64"
 	"errors"
 	"github.com/azdbaaaaaa/util/log"
 	"github.com/kirinlabs/HttpRequest"
@@ -28,14 +29,38 @@ func GetImageTypeFromURL(ctx context.Context, url string) (tp string, err error)
 		return
 	}
 
-	//buff := make([]byte, 512)
-	filetype := http.DetectContentType(b[:512])
+	contentType := http.DetectContentType(b[:512])
 
 	for i := 0; i < len(ExtList); i++ {
-		if strings.Contains(ExtList[i], filetype[6:len(filetype)]) {
+		if strings.Contains(ExtList[i], contentType[6:len(contentType)]) {
 			return ExtList[i], nil
 		}
 	}
 	return "", errors.New("Invalid image type")
 
+}
+
+func GetContentType(base64String string) (string, error) {
+	if strings.HasPrefix(base64String, "data:image/") {
+		base64String = base64String[strings.Index(base64String, ",")+1:]
+	}
+	res, err := base64.StdEncoding.DecodeString(base64String)
+	if err != nil {
+		return "", err
+	}
+	return http.DetectContentType(res[:512]), nil
+}
+
+func GetImageType(base64String string) (string, error) {
+	contentType, err := GetContentType(base64String)
+	if err != nil {
+		return "", err
+	}
+
+	for i := 0; i < len(ExtList); i++ {
+		if strings.Contains(ExtList[i], contentType[6:len(contentType)]) {
+			return ExtList[i], nil
+		}
+	}
+	return "", errors.New("invalid image type")
 }
