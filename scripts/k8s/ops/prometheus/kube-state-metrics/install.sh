7912b512a3849a60c34bed256f65bb7728d87a45
周冬彬 <zhoudongbin@ficool.com>
2022-07-27 19:51:42 +0800

    update

---
+++ scripts/k8s/ops/prometheus/kube-state-metrics/install.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# metrics see:
+# https://awesome-prometheus-alerts.grep.to/rules#kubernetes
+# install:
+# https://github.com/prometheus-community/helm-charts/tree/main/charts/kube-state-metrics#configuration
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+helm repo update
+helm install kube-state-metrics prometheus-community/kube-state-metrics -n kube-ops
